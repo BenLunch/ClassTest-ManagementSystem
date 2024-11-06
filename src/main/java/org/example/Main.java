007ae896f4f837280f71fea4e0c9e21729df32a7
G00403364@atu.ie <G00403364@atu.ie>
2024-11-06 17:20:43 +0000

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Menu for Management System/n");
+        System.out.println("-> Please choose one of the following options:/n");
+        System.out.println("-> Add Product/n");
+        System.out.println("-> Update Product/n");
+        System.out.println("-> Display All Products/n");
+        System.out.println("-> Exit/n");
+
+    }
+}
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
 public class Product {
+
+    public static void main(String[] args){
+
+        char[] ProductName;
+        double Price;
+        int Quantity;
+    }
+    ArrayList<String> ProductName = new ArrayList<String>();
+
+
 }
